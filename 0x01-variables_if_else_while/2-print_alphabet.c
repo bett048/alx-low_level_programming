d42a01c25f98c4cb7c20444d764ae106660e52e6
bett048 <pkemoi965@gmail.com>
2023-03-20 13:45:41 -0700

    1-last_digit.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - function
+ *
+ * Return:Alwaays 0
+ */
+char  main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar("\n");
+	return (0);
+}
