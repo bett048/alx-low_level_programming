df86d54ed51736348fc52f0b490fdf8743fd1b7e
bett048 <pkemoi965@gmail.com>
2023-03-20 16:25:20 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,13 +5,10 @@
  */
 int main(void)
 {
-	char n = 97;
-	char m = 65;
+	char i;
 
-	while (n <= 122)
-		putchar(n);
-	while (m <= 90)
-		putchar(m);
-	putchar("\n");
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
 	return (0);
 }
