976b0b7b052d1aab000cdc3cb5b123cbb181e779
bett048 <pkemoi965@gmail.com>
2023-03-20 17:29:55 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main -function
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10 ; i++)
+		putchar(i + 'q');
+	for (j = 'a' ; j <= 'f' ; j++)
+		putchar(j);
+	putchar('\n');
+	return (0);
+}
