c4abad47ea31b0fd2614f5d277b6af7363916d32
bett048 <pkemoi965@gmail.com>
2023-03-27 16:37:30 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,14 +8,13 @@
  */
 void print_array(int *a, int n)
 {
-	int t;
+	int i = 0;
 
-	for (t = 0; t < n; t++)
+	for (i = 0; i < n; i++)
 	{
-
-		printf("%d", a[t]);
-		if (t != n - 1)
-			printf(",");
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(", ");
 	}
 	printf("\n");
 }
