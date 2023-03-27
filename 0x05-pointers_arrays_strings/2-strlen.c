eb57562922c09a6aa61a2c8485e9ed47a944e8f6
bett048 <pkemoi965@gmail.com>
2023-03-27 14:19:20 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * strlen - return the string length
+ * @s: string whose lenth to be printed
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		count++;
+	}
+	return (0);
+}
+
